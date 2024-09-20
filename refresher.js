ce557d7ba2ee797fe562971312578d5e6e8bfc40
arpitbeuria196 <82374239+arpitbeuria196@users.noreply.github.com>
2024-09-20 19:26:20 +0100

    initial_commit

---
+++ refresher.js
@@ -0,0 +1,20 @@
+var message = "Hello"
+//var let and const
+
+/*
+1.Number
+2.Bigint
+3.String
+4.Boolean
+5.Null
+6.Undefined
+7.Symbol
+8.Object
+
+New Types - map,set,weakmap,weakset
+*/
+
+let number = 12
+let number2 = 12.345
+
+console.log(0.1+0.2);
